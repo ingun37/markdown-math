46c65e9ec73c14baed122559289aa1e6ceeee43c
Ingun Jon <ingun37@gmail.com>
2023-05-12 18:57:21 +0900

    Initial Commit

---
+++ markdown-math/markdown_mathApp.swift
@@ -0,0 +1,17 @@
+//
+//  markdown_mathApp.swift
+//  markdown-math
+//
+//  Created by Ingun Jon on 2023/05/12.
+//
+
+import SwiftUI
+
+@main
+struct markdown_mathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
