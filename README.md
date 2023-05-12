862c9104c828a968d0bb7ae7e509e6d3d35ff42b
Ingun Jon <ingun37@gmail.com>
2023-05-12 20:22:52 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#  <#Title#>
+
+https://s3-us-west-2.amazonaws.com/iink/runtime/1.3.0/MyScriptInteractiveInk-Runtime-iOS-1.3.1.zip
--- markdown-math/ContentView.swift
+++ markdown-math/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var markdownContent: String = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            TextEditor(text: $markdownContent)
         }
         .padding()
     }
