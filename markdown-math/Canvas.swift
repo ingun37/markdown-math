22e400bb218bf4a836ae9f1da0c856ccfe440925
Ingun Jon <ingun37@gmail.com>
2023-08-02 17:27:18 +0900

    canvasview deletagte

--- markdown-math/Canvas.swift
+++ markdown-math/Canvas.swift
@@ -6,15 +6,20 @@
 //
 
 import SwiftUI
+import PencilKit
 
-struct Canvas: UIViewRepresentable {
+struct Canvas: UIViewRepresentable, CanvasViewDelegate {
     func updateUIView(_ uiView: CanvasView, context: Context) {
         
     }
     
     func makeUIView(context: Context) -> CanvasView {
         let canvasView = UINib(nibName: "CanvasView", bundle: nil).instantiate(withOwner: nil).first as! CanvasView
+        canvasView.delegate = self
         return canvasView
+    }
+    func didChange(strokes: [PKStroke]) {
+        print("hehe")
     }
 }
 
--- markdown-math/CanvasView/CanvasView.swift
+++ markdown-math/CanvasView/CanvasView.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 import PencilKit
+
+protocol CanvasViewDelegate {
+    func     didChange (strokes: [PKStroke]) ;
+}
+
 class CanvasView: UIView, PKCanvasViewDelegate {
     @IBOutlet weak var pkCanvasView: PKCanvasView!;
+    var delegate: CanvasViewDelegate?
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
     func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
-        print("did change!")
+        self.delegate?.didChange(strokes: canvasView.drawing.strokes)
     }
 }
 
