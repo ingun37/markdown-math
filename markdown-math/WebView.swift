71c7f6d887c096c49f4b46dc9fc418c626951f23
Ingun Jon <ingun37@gmail.com>
2023-10-15 21:00:53 +0900

    bedounce web update

--- markdown-math/WebView.swift
+++ markdown-math/WebView.swift
@@ -9,6 +9,7 @@ import Foundation
 import Ink
 import SwiftUI
 import WebKit
+import Combine
 
 struct WebView: UIViewRepresentable {
     @Binding var markdown: String
@@ -50,23 +51,36 @@ struct WebView: UIViewRepresentable {
         return WKWebView()
     }
 
-    func updateUIView(_ uiView: WKWebView, context _: Context) {
-        let matches = markdown.matches(of: delimeter.style().inlineMatcher)
-        var newMD = ""
+    func updateUIView(_ uiView: WKWebView, context c: Context) {
+        c.coordinator.cancellable?.cancel()
+        c.coordinator.cancellable = Timer.publish(every: 0.5, on: .main, in: .default).autoconnect().first().sink { _ in
+            print("rendered!!!")
+            let matches = markdown.matches(of: delimeter.style().inlineMatcher)
+            var newMD = ""
 
-        var idx: String.Index?
-        for match in matches {
-            if idx == nil {
-                newMD += markdown[..<match.startIndex]
-            } else {
-                newMD += markdown[idx! ..< match.startIndex]
+            var idx: String.Index?
+            for match in matches {
+                if idx == nil {
+                    newMD += markdown[..<match.startIndex]
+                } else {
+                    newMD += markdown[idx! ..< match.startIndex]
+                }
+    //            print(markdown[match.startIndex..<match.endIndex].dropFirst(2).dropLast(2));
+                newMD += inlineMathBegin + markdown[match.startIndex ..< match.endIndex].dropFirst(2).dropLast(2) + inlineMathEnd
+                idx = match.endIndex
             }
-//            print(markdown[match.startIndex..<match.endIndex].dropFirst(2).dropLast(2));
-            newMD += inlineMathBegin + markdown[match.startIndex ..< match.endIndex].dropFirst(2).dropLast(2) + inlineMathEnd
-            idx = match.endIndex
+            newMD += markdown[idx!...]
+            uiView.loadHTMLString(format.header() + parser.html(from: newMD), baseURL: nil)
         }
-        newMD += markdown[idx!...]
-        uiView.loadHTMLString(format.header() + parser.html(from: newMD), baseURL: nil)
+        
+
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    class Coordinator {
+        var cancellable: Cancellable?
     }
 }
 
