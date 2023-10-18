5ee7c8d7c71c21dcc306a9043b147b814f0fde96
Ingun Jon <ingun37@gmail.com>
2023-10-18 22:47:50 +0900

    mathjax

--- markdown-math/Data.swift
+++ markdown-math/Data.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 var initialMarkdown = """
-Given a series $` \\sum^\\infin_{n=1}a_n`$ with $` a_n \\neq 0`$, the Ratio Test states that if $`(a_n)`$ satisfies
+Given a series $`\\sum^{\\infty}_{n=1} a_n`$ with $` a_n \\neq 0`$, the Ratio Test states that if $`(a_n)`$ satisfies
 
 ```math
 \\lim \\left| \\frac {a_{n+1}} {a_n} \\right| = r < 1,
