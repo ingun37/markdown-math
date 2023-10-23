88f016fed55813db612f58bbc9169b9a889941fc
Ingun Jon <ingun37@gmail.com>
2023-10-23 12:39:57 +0900

    post clone script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl -O "https://s3-us-west-2.amazonaws.com/iink/runtime/2.0.0/MyScriptInteractiveInk-Runtime-iOS-2.0.1.zip" -s
+unzip -q "MyScriptInteractiveInk-Runtime-iOS-2.0.1.zip" -d "MyScriptInteractiveInk-Runtime-iOS-2"
+#  ci_post_clone.sh
+#  markdown-math
+#
+#  Created by Ingun Jon on 10/23/23.
+#  
